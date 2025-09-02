710b3bb9feb8f0fe8c5b0d5dbd1442be46d9918a
attendspark dev <attendsparkdev@gmail.com>
2025-09-02 13:58:59 +0000

    add this features :- setup everything 3. Enable Card Generation and Sha

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.qrserver.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
